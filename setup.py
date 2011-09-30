fc770f29e9376ae06a516b3941c9bb5305b3a4b6
Marco A. Martinez <mam002@ucsd.edu>
2011-09-30 11:38:45 -0700

    Cleaned up setup file and added testing requirements.

--- setup.py
+++ setup.py
@@ -3,37 +3,34 @@ import os
 
 version = '0.3.0'
 
-setup(name='avrc.cfar.theme',
-      version=version,
-      description="Cfar theme",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+setup(
+    name='avrc.cfar.theme',
+    version=version,
+    description='Center For AIDS Research (CFAR) Plone Theme',
+    classifiers=[
+    'Framework :: Plone',
+    'Programming Language :: Python',
+    ],
+    keywords='',
+    author='BEAST Core Development Team',
+    author_email='beast@ucsd.edu',
+    url='https://github.com/beastcore/avrc.cfar.theme',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'':'src'},
+    namespace_packages=['avrc', 'avrc.cfar'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.theming',
+        'webcouturier.dropdownmenu'
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir={'':'src'},
-      namespace_packages=['avrc', 'avrc.cfar'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.theming',
-          'webcouturier.dropdownmenu'
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    extras_require=dict(
+        test=['plone.app.testing'],
+        ),
+    entry_points="""    
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
